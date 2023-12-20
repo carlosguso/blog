d448417f178ffb0607d6e28010b26e56f02e47b2
Carlos Gutiérrez <33915304+carlosguso@users.noreply.github.com>
2023-12-19 21:07:53 -0600

    Setup a Project structure (#2)
    Co-authored-by: Carlos Gutierrez Solis <carlosguso@protonmail.com>

---
+++ src/app/blog/posts.ts
@@ -0,0 +1,9 @@
+export const posts = {
+    "2023": [
+        {
+            title: "My First Post",
+            url: "2023/my_first_post",
+            creationDate: "2023-12-19",
+        },
+    ],
+};
