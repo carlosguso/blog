80408ba2a981795c04f5c150499b8c242589b103
Carlos Gutierrez Solis <carlosguso@protonmail.com>
2024-05-06 20:34:46 -0600

    Extend some base colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "main-dark": "#1a0e24",
+        "main-accent": "#a96ad9",
       },
     },
   },
