fa15fe07f373cf54e9a1fc745f0130bf007e1f9c
Carlos Gutierrez Solis <carlosguso@protonmail.com>
2024-08-13 22:33:50 -0600

    This is like 7 months old can't remember

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Merriweather_Sans } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Merriweather_Sans } from "next/font/google";
+import "./globals.css";
 
-const merriweatherSans = Merriweather_Sans({ subsets: ['latin'], weight: ['300', '600'] })
+const merriweatherSans = Merriweather_Sans({
+  subsets: ["latin"],
+  weight: ["300", "600"],
+});
 
 export const metadata: Metadata = {
-  title: 'Carlos Gutierrez',
+  title: "Carlos Gutierrez",
   description: "Carlos' personal page",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" >
-      <body className={`${merriweatherSans.className} bg-main-dark`}>{children}</body>
+    <html lang="en">
+      <body className={`${merriweatherSans.className} bg-main-dark`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
