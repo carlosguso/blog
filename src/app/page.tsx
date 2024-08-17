1628076dc0a92cfbdd46373e88db26a1c767df35
Carlos Gutierrez Solis <carlosguso@protonmail.com>
2024-08-16 20:01:14 -0600

    Add conditional rendering for the highlights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,19 @@ import Link from "next/link";
 import About from "./about.mdx";
 import highlights from "./highlights.json";
 
+type Highlights = {
+  thoughts: Highlight[];
+  articles: Highlight[];
+  projects: Highlight[];
+};
+
+const typedHighlights = highlights as Highlights;
+
+type Highlight = {
+  title: string;
+  description: string;
+};
+
 export default function Landing() {
   return (
     <div className="w-full flex justify-center align-center">
@@ -40,56 +53,68 @@ export default function Landing() {
           <About />
         </section>
         {/* Think about putting the image here */}
-        <section className="mt-12">
-          <h2 className="text-2xl font-semibold mb-6 text-main-accent">
-            Thoughts
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {highlights.thoughts.map((thought, index) => (
-              <div
-                key={index}
-                className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-              >
-                <h3 className="text-xl font-semibold mb-2">{thought.title}</h3>
-                <p className="text-gray-300">{thought.description}</p>
-              </div>
-            ))}
-          </div>
-        </section>
+        {typedHighlights.thoughts.length > 0 && (
+          <section className="mt-12">
+            <h2 className="text-2xl font-semibold mb-6 text-main-accent">
+              Thoughts
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {typedHighlights.thoughts.map((thought, index) => (
+                <div
+                  key={index}
+                  className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                >
+                  <h3 className="text-xl font-semibold mb-2">
+                    {thought.title}
+                  </h3>
+                  <p className="text-gray-300">{thought.description}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
 
-        <section className="mt-12">
-          <h2 className="text-2xl font-semibold mb-6 text-main-accent">
-            Articles
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {highlights.articles.map((article, index) => (
-              <div
-                key={index}
-                className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-              >
-                <h3 className="text-xl font-semibold mb-2">{article.title}</h3>
-                <p className="text-gray-300">{article.description}</p>
-              </div>
-            ))}
-          </div>
-        </section>
+        {typedHighlights.articles.length > 0 && (
+          <section className="mt-12">
+            <h2 className="text-2xl font-semibold mb-6 text-main-accent">
+              Articles
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {typedHighlights.articles.map((article, index) => (
+                <div
+                  key={index}
+                  className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                >
+                  <h3 className="text-xl font-semibold mb-2">
+                    {article.title}
+                  </h3>
+                  <p className="text-gray-300">{article.description}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
 
-        <section className="mt-12">
-          <h2 className="text-2xl font-semibold mb-6 text-main-accent">
-            Projects
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {highlights.projects.map((project, index) => (
-              <div
-                key={index}
-                className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-              >
-                <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-                <p className="text-gray-300">{project.description}</p>
-              </div>
-            ))}
-          </div>
-        </section>
+        {typedHighlights.projects.length > 0 && (
+          <section className="mt-12">
+            <h2 className="text-2xl font-semibold mb-6 text-main-accent">
+              Projects
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {typedHighlights.projects.map((project, index) => (
+                <div
+                  key={index}
+                  className="bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                >
+                  <h3 className="text-xl font-semibold mb-2">
+                    {project.title}
+                  </h3>
+                  <p className="text-gray-300">{project.description}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
         <footer className="mt-8 pt-8 border-t border-gray-200">
           <div className="flex justify-center space-x-6">
             <Link
