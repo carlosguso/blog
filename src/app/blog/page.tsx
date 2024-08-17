ed73630fe1635952b034f597220f53b7d6343ae7
Carlos Gutierrez Solis <carlosguso@protonmail.com>
2024-08-17 16:31:52 -0600

    Implement empty list logic. Will remove once all section have content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,21 +12,27 @@ export default function BlogMainPage() {
           <h2 className="text-2xl font-semibold mb-6 text-main-accent-light dark:text-main-accent">
             Thoughts
           </h2>
-          <div className="flex flex-col space-y-6">
-            {blogPosts.posts.map((post, index) => (
-              <Link href={post.url} key={index} className="block">
-                <div className="bg-white dark:bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                  <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
-                  <p className="text-gray-700 dark:text-gray-300">
-                    {post.description}
-                  </p>
-                  <p className="text-sm text-gray-500 dark:text-gray-400 mt-2">
-                    {post.date}
-                  </p>
-                </div>
-              </Link>
-            ))}
-          </div>
+          {blogPosts.posts.length > 0 ? (
+            <div className="flex flex-col space-y-6">
+              {blogPosts.posts.map((post, index) => (
+                <Link href={post.url} key={index} className="block">
+                  <div className="bg-white dark:bg-[#1e0a30] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+                    <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
+                    <p className="text-gray-700 dark:text-gray-300">
+                      {post.description}
+                    </p>
+                    <p className="text-sm text-gray-500 dark:text-gray-400 mt-2">
+                      {post.date}
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-700 dark:text-gray-300">
+              No blog posts available at the moment. Check back later!
+            </p>
+          )}
         </section>
         <Footer />
       </div>
