9295db25d2db9374216909cda68e16fa14e8f765
Carlos Gutiérrez <33915304+carlosguso@users.noreply.github.com>
2024-08-19 22:12:19 -0600

    Mobile reponsive fixes (#10)
    Co-authored-by: Carlos Gutierrez Solis <carlosguso@protonmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center pt-12 pb-3">
+    <header className="flex flex-col sm:flex-row justify-between items-center pt-8 pb-3 px-4 sm:px-0">
       <Link
         href="/"
-        className="transition-colors duration-300 ease-in-out hover:text-main-accent-light dark:hover:text-main-accent rounded-md text-sx"
+        className="transition-colors duration-300 ease-in-out hover:text-main-accent-light dark:hover:text-main-accent rounded-md text-sx mb-4 sm:mb-0"
       >
         <h1 className="text-2xl font-semibold text-main-accent-light dark:text-main-accent">
           Carlos Gutierrez
         </h1>
       </Link>
-      <div className="flex items-center space-x-4">
+      <nav className="flex items-center space-x-4">
         <Link
           href="/blog"
           className="transition-colors duration-300 ease-in-out hover:text-main-accent-light dark:hover:text-main-accent rounded-md text-sx"
@@ -32,7 +32,7 @@ const Header = () => {
           Articles
         </Link>
         <ThemeToggle />
-      </div>
+      </nav>
     </header>
   );
 };
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
 }) {
   return (
     <div className="w-full flex justify-center align-center">
-      <div className="lg:w-8/12 md:w-9/12 w-full p-2 mx-auto sm:px-4">
+      <div className="lg:w-8/12 md:w-9/12 w-full px-4 sm:px-6 md:px-8 mx-auto">
         <Header />
         <main className="mt-12">{children}</main>
         <Footer />
