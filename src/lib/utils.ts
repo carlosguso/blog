7b258b70e04950167d20a35b1891e4f491a341a5
Carlos Gutierrez <carlosguso@protonmail.com>
2025-12-30 12:31:19 -0600

    Make the process MDX files a util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export interface MDXPost {
+  title: string;
+  date: string;
+  slug: string;
+}
+
+export function processMDXFiles(
+  files: Array<{ url: string; frontmatter: { title?: string; date?: string } }>
+): MDXPost[] {
+  return files
+    .map((file) => {
+      const url = file.url;
+      const slug = url.split('/').pop()?.replace('.html', '').replace(/\/$/, '') || '';
+      return {
+        title: file.frontmatter.title || '',
+        date: file.frontmatter.date || '',
+        slug,
+      };
+    })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+}
